e6024a6b63f3be061367c1d99401bb3e7336e92e
Kedar Kulkarni <kulkarnikk07@gmail.com>
2024-04-29 19:07:53 -0700

    2 more problems solved

--- Sql3 Combined.sql
+++ Sql3 Combined.sql
@@ -9,7 +9,30 @@ and l2.num = l3.num
 
 -- 2 Problem 2 :Number of Passengers in Each Bus 	(	https://leetcode.com/problems/the-number-of-passengers-in-each-bus-i/ )
 
+with CTE as(
+select p.passenger_id, p.arrival_time, min(b.arrival_time) as "btime"
+from Passengers p
+    inner join Buses b
+on p.arrival_time <= b.arrival_time
+group by p.passenger_id #,p.arrival_time
+)
+select b.bus_id, count(c.btime) "passengers_cnt"
+from Buses b
+left join CTE c
+on b.arrival_time = c.btime
+   group by b.bus_id
+    order by b.bus_id;
+
 -- 3 Problem 3 :User Activity		(https://leetcode.com/problems/user-activity-for-the-past-30-days-i/ )
+select distinct activity_date as day, count(distinct user_id) active_users
+from Activity
+where 
+#activity_date between date_add('2019-07-27', interval -29 day) and '2019-07-27'
+#activity_date between date_sub('2019-07-27', interval 29 day) and '2019-07-27'
+DATEDIFF('2019-07-27',activity_date) BETWEEN 0 AND 29
+#activity_date between '2019-06-28' and '2019-07-27'
+group by activity_date
+order by activity_date
 
 -- 4 Problem 4 :Dynamic Pivoting of a Table	(	https://leetcode.com/problems/dynamic-pivoting-of-a-table/ )
 
